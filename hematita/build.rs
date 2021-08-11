7ddaa8194b2ef72b6e99fcd7d7a03c5a30464e1a
Daniel Conley <himself@danii.dev>
2021-08-11 15:22:14 -0400

    Add LICENSE, Fix Code Gen & README Key For Crates.io

--- hematita/build.rs
+++ hematita/build.rs
@@ -3,15 +3,16 @@
 use self::tests::matches_standard;
 
 pub mod tests {
-	use std::env::current_dir;
+	use std::env::{current_dir, var};
 
 	pub mod matches_standard {
 		include!("./tests/matches_standard/build.rs");
 	}
 
 	pub fn matches_standard() {
+		let build = var("OUT_DIR").unwrap();
 		let current = current_dir().unwrap().join("tests/matches_standard/");
-		matches_standard::build(&current);
+		matches_standard::build(&current, build.as_ref());
 	}
 }
 
